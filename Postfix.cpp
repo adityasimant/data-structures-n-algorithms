a7ddf8ee58a3d91dd3b36aa5586e5ba8000121f4
Aditya simant <adityasimant88@gmail.com>
2022-03-07 19:18:08 +0530

    commit

---
+++ Postfix.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class stk
+{
+private:
+    int top;
+    char list[60];
+    int max = 60;
+
+public:
+    stk()
+    {
+        top = -1;
+    }
+    bool isFull()
+    {
+        bool f = false;
+        if (top == max - 1)
+        {
+            f = true;
+        }
+        return f;
+    }
+    bool isEmpty()
+    {
+        bool f = false;
+        if (top < 0)
+        {
+            f = true;
+        }
+        return f;
+    }
+    void push(char a)
+    {
+        if (isFull())
+        {
+            cout << "the stack is full" << endl;
+            return;
+        }
+
+        list[++top] = a;
+    }
+    char pop()
+    {
+        if (isEmpty())
+        {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+        char ch = list[top--];
+        return ch;
+    }
+    char peek()
+    {
+
+        return list[top];
+    }
+};
+
+int getPrecd(char r)
+{
+    if (r == '+' || r == '-')
+    {
+        return 1;
+    }
+    if (r == '*' || r == '/')
+    {
+        return 2;
+    }
+}
+
+int main()
+{
+    stk s1;
+    char input[60], output[60];
+    int j = 0;
+    cout << "enter the infix : " << endl;
+    cin >> input;
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (input[i] >= 97 && input[i] <= 122)
+        {
+            output[j] = input[i];
+            j++;
+            // cout<<input[i];
+        }
+
+        else if (input[i] == '(')
+        {
+            s1.push(input[i]);
+        }
+        else if (input[i] == ')')
+        {
+            char ch1;
+            do
+            {
+                ch1 = s1.pop();
+                output[j] = ch1;
+                j++;
+            } while (ch1 != '(');
+
+        }
+
+        else
+        {
+
+            if (!s1.isEmpty() && getPrecd(s1.peek()) >= getPrecd(input[i]))
+            {
+                char end;
+                end = s1.pop();
+                output[j] = end;
+                j++;
+                // cout<<s1.pop();
+            }
+
+            s1.push(input[i]);
+        }
+    }
+    while (!s1.isEmpty())
+    {
+
+        char last = s1.pop();
+        output[j] = last;
+        j++;
+        // cout<<s1.pop();
+    }
+
+    int len = strlen(output);
+    for (int k = 0; k < strlen(input); k++)
+    {
+        cout << output[k];
+    }
+    
+    return 0;
+}
